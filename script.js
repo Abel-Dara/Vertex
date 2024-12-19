b2e53d5890ae851ff7f593d3260e943478225f20
Abel Dara <abelojeyinka3@gmail.com>
2024-12-19 16:38:03 +0100

    Update script.js

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ var x = setInterval(function() {
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
     const newLocal = Math.floor((distance % (10 * 60)) / 10);
 
-    document.getElementById("countdown").innerHTML ='MY GOAL' +  days  + "d " + hours + " h" + minutes + " m"  +seconds + " s" ;
+    document.getElementById("countdown").innerHTML ='MY GOAL ' +  days  + "D " + hours + "H " + minutes + " m"  +seconds + "s" ;
 
     if (distance < 0) {
         clearInterval(x);
@@ -28,11 +28,63 @@ var x = setInterval(function() {
 function toggleSidebar() {
   const sidebar = document.getElementById("sidebar");
   sidebar.classList.toggle("open");
+  sidebar.classList.Menu("close")
 
   const content = document.querySelector(".content");
   if (sidebar.classList.contains("open")) {
-    content.style.marginLeft = "250px";
+    content.style.marginRight = "250px";
   } else {
-    content.style.marginLeft = "0";
+    content.style.marginRight = "0";
   }
 }
+
+
+
+
+
+
+
+var countDownDate = new Date("jul 8, 2029 23:59:59").getTime();
+
+
+var x = setInterval(function() {
+
+    var now = new Date().getTime();
+
+
+    var distance = countDownDate - now;
+
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    const newLocal = Math.floor((distance % (10 * 60)) / 10);
+
+    document.getElementById("countdown").innerHTML ='MY GOAL ' +  days  + "D " + hours + "H " + minutes + " m"  +seconds + "s" ;
+
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById(" +' '+ countdown").innerHTML = "EXPIRED";
+    }
+}, 1000);
+
+
+
+function toggleSidebar() {
+  const sidebar = document.getElementById("sidebar");
+  sidebar.classList.toggle("open");
+  sidebar.classList.Menu("close")
+
+  const content = document.querySelector(".content");
+  if (sidebar.classList.contains("open")) {
+    content.style.marginRight = "250px";
+  } else {
+    content.style.marginRight = "0";
+  }
+}
+
+
+
+
+
+
